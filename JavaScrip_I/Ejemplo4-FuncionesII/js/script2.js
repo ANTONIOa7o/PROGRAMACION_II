1f0d397c4217e781d2d927419c1bda73f9bd4db9
Antonio <78384046+ANTONIOa7o@users.noreply.github.com>
2022-05-06 10:32:07 +0200

    Add files via upload

---
+++ JavaScrip_I/Ejemplo4-FuncionesII/js/script2.js
@@ -0,0 +1,15 @@
+
+
+
+function mensaje(){
+    
+    var miNombre =document.getElementById('nombre').value;
+ 
+    var misApellidos =document.getElementById('apellidos').value;
+
+    document.getElementById('resultado').innerHTML="Acceso Autorizado a:" 
+    + miNombre+" "+misApellidos+" \n"+ "Bienvenido al Área Virtual";
+
+
+
+}
